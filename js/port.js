8b80a9e0b7ff3454cfa93d3aa7845a131be64fc6
Aaditya Sharma <121788496+Aadi13-cell@users.noreply.github.com>
2023-08-09 10:17:26 +0530

    Delete js directory

--- js/port.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.querySelector(".hamb");
-var navlist = document.querySelector(".nav-list");
-var link = document.querySelector(".nav-list li");
-
-hamburger.addEventListener("click", function(){
-	this.classList.toggle("click");
-	navlist.classList.toggle("open");
-
-});
